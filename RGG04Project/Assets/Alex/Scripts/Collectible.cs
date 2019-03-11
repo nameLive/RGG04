dd872bdb3f9c69c9cf8b80dce71f14613ac9ca2a
Alexander Flodén <alexander.floden@futuregames.se>
2019-03-11 14:05:24 +0100

    # Basic Setup of Collectibles and PlayerState

---
+++ RGG04Project/Assets/Alex/Scripts/Collectible.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour {
+
+    public int amountOfCollectibles;
+
+    public bool isPowerUp;
+    public float powerUpLengthOfTime;
+
+
+    public void OnTriggerEnter2D (Collider2D other) {
+
+        if (other.gameObject.tag == "Player") {
+
+            if (!isPowerUp)
+                other.GetComponent<PlayerPickup>().PickupCollectible(amountOfCollectibles);
+            else
+                other.GetComponent<PlayerPickup>().PowerUp(powerUpLengthOfTime);
+
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ RGG04Project/Assets/Alex/Scripts/PlayerPickup.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPickup : MonoBehaviour {
+
+    public Text amountOfDonuts;
+
+    enum PlayerState {Normal, Powered, Killed};
+
+    PlayerState playerState = PlayerState.Normal;
+
+    public int amountOfCollectiblesPickedUp;
+
+    public int minAmountOfCollectiblesRequired;
+    
+
+    void Start() {
+        
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void PickupCollectible(int amount) {
+        
+        amountOfCollectiblesPickedUp += amount;
+
+        amountOfDonuts.text = "Donuts: " + amountOfCollectiblesPickedUp;
+
+        if (amountOfCollectiblesPickedUp >= minAmountOfCollectiblesRequired) {
+
+        }
+    }
+
+    public void PowerUp(float amountOfTimePoweredUp) {
+
+        if (playerState == PlayerState.Normal) {
+
+            playerState = PlayerState.Powered;
+        }
+    }
+}
