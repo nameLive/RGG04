b455fadcc6c3e44ce01a85d5a0cfbc60cdc140e9
JosieFolke <48436986+JosieFolke@users.noreply.github.com>
2019-03-12 16:20:06 +0100

    # Implemented wall jumping if we want it (commented out)
    # More refined jumping
    # Commented out wall jumping (still buggy! doesn't apply x and y force at the same time)

--- RGG04Project/Assets/Josie/PlayerMovement.cs
+++ RGG04Project/Assets/Josie/PlayerMovement.cs
@@ -4,38 +4,73 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb2d;
+
     public float speed = 1f;
     public string horizontalKey = "Horizontal";
+    public bool facingRight;
 
+    [Header("Jumping")]
     public float jumpVelocity;
     public string jumpKey = "Jump";
-    bool jumpRequest;
     bool grounded;
+    bool canDoubleJump;
+
+    /* [Header("Wall Jumping")]
+    public bool wallSliding;
+    public Transform wallCheckPoint;
+    public bool wallCheck;
+    public LayerMask wallLayerMask;
+    public float wallPushOff;
+    public float wallPushUp; */
+
+
+    public void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
 
 
     private void Update()
     {
-        float horizontal = Input.GetAxis(horizontalKey);
+        float horizontal = Input.GetAxis(horizontalKey); 
         if (!Mathf.Approximately(horizontal, 0f))
         {
             Move(horizontal);
         }
 
-        if (Input.GetButtonDown(jumpKey) && grounded)
+        if (Input.GetButtonDown(jumpKey) /* && !wallSliding */)
         {
-            jumpRequest = true;
+            Jump();
         }
 
-    }
-
-    void FixedUpdate()
-    {
-        if (jumpRequest)
+        /*
+        if (!grounded)
         {
-            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpVelocity, ForceMode2D.Impulse);
+            wallCheck = Physics2D.OverlapCircle(wallCheckPoint.position, 0.1f, wallLayerMask);
 
-            jumpRequest = false;
+            if (facingRight && horizontal > 0f || !facingRight && horizontal < 0f)
+            {
+                if (wallCheck)
+                {
+                    WallSliding();
+                }
+
+            }
+          
+
         }
+
+        if (wallCheck == false || grounded)
+        {
+            wallSliding = false;
+        }
+
+        if (grounded)
+        {
+            wallCheck = false;
+        } */
+
     }
 
 
@@ -48,12 +83,36 @@ public class PlayerMovement : MonoBehaviour
         if (horizontal > 0)
         {
             transform.localScale = new Vector2(1.840318f, 1.813247f);
+            facingRight = true;
         }
 
         else if (horizontal < 0)
         {
             transform.localScale = new Vector2(-1.840318f, 1.813247f);
+            facingRight = false;
         }
+    }
+
+
+    #region JUMPING
+    private void Jump()
+    {
+        if (grounded)
+        {
+            rb2d.AddForce(Vector2.up * jumpVelocity, ForceMode2D.Impulse);
+            canDoubleJump = true;
+        }
+
+        else
+        {
+            if (canDoubleJump)
+            {
+                canDoubleJump = false;
+                rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
+                rb2d.AddForce(Vector2.up * jumpVelocity, ForceMode2D.Impulse);
+            }
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -71,4 +130,26 @@ public class PlayerMovement : MonoBehaviour
             grounded = false;
         }
     }
+
+    #endregion JUMPING
+
+/* WALL JUMPING IF WE WANT
+void WallSliding()
+    {
+        rb2d.velocity = new Vector2(rb2d.velocity.x, -0.7f);
+        wallSliding = true;
+
+        if (Input.GetButtonDown(jumpKey))
+        {
+            if (facingRight)
+            {
+               rb2d.AddForce(new Vector2(-wallPushOff, wallPushUp) * jumpVelocity, ForceMode2D.Impulse);
+            }
+
+            else
+            {
+                rb2d.AddForce(new Vector2(wallPushOff, wallPushUp) * jumpVelocity, ForceMode2D.Impulse);
+            }
+        }
+    } */
 }
