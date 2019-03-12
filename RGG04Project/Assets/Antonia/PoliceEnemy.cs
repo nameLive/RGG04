3825752cc4886b3b789c9ee3b5dadb3ba5e15d8f
Antonia Kiili <Antonia.kiili@futuregames.nu>
2019-03-12 11:35:19 +0100

    Police Enemy(First Pass)
    The enemy works as intended now. Can tweak this later if needed.

--- RGG04Project/Assets/Antonia/PoliceEnemy.cs
+++ RGG04Project/Assets/Antonia/PoliceEnemy.cs
@@ -4,76 +4,101 @@ using UnityEngine;
 
 public class PoliceEnemy : MonoBehaviour
 {
-	[SerializeField]
-	GameObject targetLocationVisualizer;
 
 	[SerializeField]
 	GameObject player;
 
 	[SerializeField]
-	float distanceToPlayerThreshHold = 5f;
+	[Tooltip("The enemy will always keep this distance to the player no matter how fast the player moves")]
+	float maxDistanceToPlayer = 20f;
+
+	[SerializeField]
+	[Tooltip("The speed of which the enemy moves towards the player when in range")]
+	[Range(0.0f, 0.5f)]
+	float movementSpeed = 0.03f;
 
 	float distanceToPlayer = 0f;
-	float alpha = 1f;
-	bool reachedTargetLocation = true;
 
 	Vector3 directionToPlayer = new Vector3();
-	Vector3 targetLocation = new Vector3();
-	Vector3 startLocation = new Vector3();
+	Vector3 targetPosition = new Vector3();
+	Vector3 startPosition = new Vector3();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-		//CalculateTargetPosition();
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		CalculateTargetPosition();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-		directionToPlayer = player.transform.position - transform.position;
-
-		distanceToPlayer = directionToPlayer.magnitude;
-
-		//Debug.Log("Distance to player:" + distanceToPlayer);
-
-			if (alpha >= 1f)
-			{
-				CalculateTargetPosition();
-				reachedTargetLocation = false;
-			}
-
-			MoveTowardsPlayer();
-			Debug.Log("Alpha: " + alpha);
-	
-
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		CalculateTargetPosition();
+		MoveTowardsPlayer();
+	}
 
 	void CalculateTargetPosition()
 	{
-		alpha = 0f;
-		startLocation = transform.position;
-		targetLocation = directionToPlayer / directionToPlayer.magnitude;
-		//targetLocation = targetLocation / 5f;
-		//targetLocation += startLocation;
-		targetLocationVisualizer.transform.position = targetLocation;
+
+		startPosition = transform.position;
+		targetPosition = player.transform.position;
+
 	}
 
 
 	void MoveTowardsPlayer()
 	{
-		if (transform.position == targetLocation)
+		if (HitThresholdDistanceToPlayer())
 		{
-			reachedTargetLocation = true;
+			return;
 		}
 
-		Debug.DrawLine(transform.position, targetLocation, Color.red);
+		if (IsWithinPlayerRange())
+		{
+			Vector3 NewPosition = Vector3.MoveTowards(transform.position, targetPosition, movementSpeed);
+			transform.position = NewPosition;
+		}
+		else
+		{
+			//Calculates the exact point of maxDistanceToPlayer(20) units from the player pointing towards the enemy
+			Vector3 PlayerToEnemyPos = transform.position - player.transform.position;
+			PlayerToEnemyPos = PlayerToEnemyPos / PlayerToEnemyPos.magnitude;
+			PlayerToEnemyPos *= maxDistanceToPlayer;
+			Vector3 EndPos = player.transform.position + PlayerToEnemyPos;
 
+			transform.position = EndPos;
+		}
+	
 
-		Vector3 NewPosition = Vector3.Lerp(transform.position, targetLocation, alpha);
+	}
 
-		alpha += Time.deltaTime/5f;
+	bool IsWithinPlayerRange()
+	{
+		directionToPlayer = player.transform.position - transform.position;
+		distanceToPlayer = directionToPlayer.magnitude;
 
-		transform.position = NewPosition;
+		if (distanceToPlayer <= maxDistanceToPlayer)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	bool HitThresholdDistanceToPlayer()
+	{
+		directionToPlayer = player.transform.position - transform.position;
+		distanceToPlayer = directionToPlayer.magnitude;
+
+		if (distanceToPlayer >= 1f)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
 
 	}
 }
