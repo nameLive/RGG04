86e7e74b351cb6856e522f51a18b1124a3c37c4f
Antonia Kiili <Antonia.kiili@futuregames.nu>
2019-03-13 15:50:58 +0100

    Structured a base system for the collectibles and added prefabs
    All the collectibles have their proper prefabs setut now in the "Prefabs" folder. And they have their own belonging scripts in the "Scripts" folder. Also Enums have their own separate files(for clarity)

---
+++ RGG04Project/Assets/Scripts/PlayerStateHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//There should ever be only one instance of this script, and that is on the player.
+public class PlayerStateHandler : MonoBehaviour
+{
+    public delegate void OnPlayerStateChanged();
+    OnPlayerStateChanged OnHammerState;
+    OnPlayerStateChanged OnNormalState;
+
+    PlayerState currentPlayerState = PlayerState.Normal;
+
+    //public event called from other scripts(e.g collectibles) to try to change the state. Returns true if state was successfully changed.
+    public bool SetNewState(PlayerState NewState)
+    {
+        ChangeState(NewState);
+
+        return true;
+    }
+
+    //Internal event that actually changes the state
+    void ChangeState(PlayerState NewState)
+    {
+        currentPlayerState = NewState;
+
+        if (NewState == PlayerState.Hammering)
+        {
+            OnHammerState();
+        }
+        else if (NewState == PlayerState.Normal)
+        {
+            OnNormalState();
+        }
+    }
+}
