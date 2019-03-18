82758101a9ecdd243717d2ada6e24a9fc0059877
Antonia Kiili <Antonia.kiili@futuregames.nu>
2019-03-18 17:11:31 +0100

    Updated the enemy to be bobbing

---
+++ RGG04Project/Assets/Scripts/Enemy/EnemyBobbingMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBobbingMovement : BobbingMovement
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemyStunState enemyStunState = GetComponent<EnemyStunState>();
+        enemyStunState.EventOnBeginStun += BeginStun;
+        enemyStunState.EventOnEndStun += EndStun;
+    }
+
+    void BeginStun()
+    {
+        shouldBob = false;
+    }
+
+    void EndStun()
+    {
+        shouldBob = true;
+    }
+
+}
