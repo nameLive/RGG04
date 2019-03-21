c12d9faee398f65740dc08dfb3713ac3e696e255
Antonia Kiili <Antonia.kiili@futuregames.nu>
2019-03-21 09:59:08 +0100

    Removed the old "Hammer sprite" placeholder and cleaned up the code

--- RGG04Project/Assets/Scripts/Player/PlayerHammerState.cs
+++ RGG04Project/Assets/Scripts/Player/PlayerHammerState.cs
@@ -7,27 +7,17 @@ public class PlayerHammerState : MonoBehaviour
 
 	Damager damager;
 
-	bool hammerState = false;
     public bool hasHammer;
-
-	[SerializeField]
-	SpriteRenderer hammerSprite;
 
 	PlayerMovement playerMovement;
 
 	float facingLeftXOffset = -1f;
 	float facingRightXOffset = 1f;
 
-	Quaternion facingLeftSpriteRotation;
-	Quaternion facingRightSpriteRotation;
-
 	private void Start()
 	{
 		facingLeftXOffset = gameObject.transform.localPosition.x;
 		facingRightXOffset = facingLeftXOffset * -1;
-
-		//facingLeftSpriteRotation = hammerSprite.gameObject.transform.localRotation;
-		//facingRightSpriteRotation = Quaternion.Inverse(facingLeftSpriteRotation);
 
 		PlayerStateHandler myHandler = gameObject.GetComponentInParent<PlayerStateHandler>();
 		myHandler.OnHammerState += StartHammerState;
@@ -47,7 +37,7 @@ public class PlayerHammerState : MonoBehaviour
 
 	private void Update()
 	{
-		if (hammerState)
+		if (hasHammer)
 		{
 			float targetXOffset = playerMovement.facingRight ? facingRightXOffset : facingLeftXOffset;
 
@@ -55,35 +45,26 @@ public class PlayerHammerState : MonoBehaviour
 			{
 				Vector3 targetPosition = new Vector3(targetXOffset, 0f, 0f);
 				gameObject.transform.localPosition = targetPosition;
-
-				Quaternion targetRotation = playerMovement.facingRight ? facingRightSpriteRotation : facingLeftSpriteRotation;
-
-
-				//hammerSprite.gameObject.transform.localRotation = targetRotation;
 			}
 		}
 	}
 
 	void StartHammerState()
 	{
-		if (!hammerState)
+		if (!hasHammer)
 		{
             Debug.Log("Start Hammer state");
-			hammerState = true;
 			damager.canDealDamage = true;
-			hammerSprite.enabled = true;
             hasHammer = true; 
 		}
 	}
 
 	void EndHammerState()
 	{
-		if (hammerState)
+		if (hasHammer)
 		{
             Debug.Log("End hammer state");
-			hammerState = false;
 			damager.canDealDamage = false;
-			hammerSprite.enabled = false;
             hasHammer = false;
 		}
 
