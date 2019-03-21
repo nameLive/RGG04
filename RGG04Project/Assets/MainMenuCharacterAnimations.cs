3de6971a870a9cf09ea86c959608022783256a57
DESKTOP-HVVKBA2\alexa <Alexander_Floden@hotmail.com>
2019-03-21 04:12:55 +0100

    # Added Intro cutscene

---
+++ RGG04Project/Assets/MainMenuCharacterAnimations.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCharacterAnimations : MonoBehaviour {
+
+    Animator animator;
+
+    bool run;
+    
+    void Start() {
+
+        animator = GetComponent<Animator>();
+        
+        StartCoroutine(DelayToStartAnAnimation(7f, "IsBeingChased", true));
+    }
+
+    //--------------------------------------
+
+    IEnumerator DelayToStartAnAnimation(float delay, string animation, bool Start) {
+
+        yield return new WaitForSeconds(delay);
+
+        run = true;
+
+        animator.SetBool(animation, Start);
+    }
+
+    void Update() {
+
+        if (run)
+            transform.Translate(new Vector3(7.5f * Time.deltaTime, 0, 0), Space.World);
+    }
+}
--- RGG04Project/Assets/Scripts/Menus/MainMenu.cs
+++ RGG04Project/Assets/Scripts/Menus/MainMenu.cs
@@ -6,12 +6,31 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour {
 
     GameManager gameManager;
-    
+
+    public GameObject StartGameButton;
+
+    public GameObject QuitButton;
+
     //----------------------------------------------
 
     void Start() {
 
+        StartGameButton.SetActive(false);
+
+        QuitButton.SetActive(false);
+
         gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+        StartCoroutine(ButtonDelay(2));
+    }
+
+    IEnumerator ButtonDelay (float delay) {
+
+        yield return new WaitForSeconds(delay);
+
+        StartGameButton.SetActive(true);
+
+        QuitButton.SetActive(true);
     }
 
     //----------------------------------------------
